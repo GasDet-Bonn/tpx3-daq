a04df10c02495f4e7455592d6c7e8771fb3865c5
Sebastian Schmidt <s.schmidt@physik.uni-bonn.de>
2018-04-11 13:57:46 +0200

    ENH: add `bitwise` argument to toByteList to get str repr of bytes

--- tpx3/utils.py
+++ tpx3/utils.py
@@ -1,19 +1,24 @@
 
-# this should really be a class method of BitLogic, but to stay compatible with 
+# this should really be a class method of BitLogic, but to stay compatible with
 # the current basil version, we add it at runtime
-def toByteList(self):
+def toByteList(obj, bitwise = False):
     """
     Converts bitstring to a list of bytes
+    If `bitwise` == True, we return a list of strings containing the binary repr
+    of the bytes.
     """
-    if self.length() % 8 != 0:
-        raise ValueError("""Cannot convert to array of bytes, if number of 
+    if obj.length() % 8 != 0:
+        raise ValueError("""Cannot convert to array of bytes, if number of
         bits not a multiple of a byte""")
-    nbytes = self.length() / 8
+    nbytes = obj.length() / 8
     byteList = []
 
     # range from 0 to 40, reversed to get MSB first
     # for some reason list comprehension doesn't work here?
-    for i in reversed(range(0, self.length(), 8)):
-        byteList += [self[i+7:i].tovalue()]
+    for i in reversed(range(0, obj.length(), 8)):
+        if bitwise == False:
+            byteList += [obj[i+7:i].tovalue()]
+        else:
+            byteList += [obj[i+7:i].__str__()]
 
     return byteList
