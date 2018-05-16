ef80ffb66bfa8b83f23608ebd4c93dc7ce01200d
vedant8 <vedant8basu@gmail.com>
2018-05-16 17:22:27 +0200

    Update testreadpcr.py

--- tests/testreadpcr.py
+++ tests/testreadpcr.py
@@ -47,7 +47,10 @@ def read_pcr():
 
 
     #Step 4: Send Read Pixel Configuration Register Command
-    chip.read_pixel_config_reg(255,False)
+    a=1
+    for i in range(4):
+     	a=a *16+12
+    chip.read_pixel_config_reg(a,False)
     chip['FIFO'].reset()
     time.sleep(0.01)
     chip.write(data)
@@ -55,20 +58,27 @@ def read_pcr():
    
 
     #Step 5: Send Read Pixel Matrix Sequential command
-	chip.read_pixel_matrix_sequential(255,False)
+    x=2
+    for i in range(4):
+     	x=x * 4 + 2
+	chip.read_pixel_matrix_sequential(x,False)
     chip['FIFO'].reset()
     time.sleep(0.01)
     chip.write(data)
     time.sleep(0.01)
 
-    #Step 6: Receive Data    
-    fdata = chip['FIFO'].get_data()
-    print fdata
-    dout = chip.decode_fpga(fdata, True)
-    print dout
-    for i, d in enumerate(fdata):
-        print i, hex(d), (d & 0x01000000) != 0, bin(d & 0xffffff), hex(d & 0xffffff)
-        pretty_print(d)
-    for el in dout:
-        print "Decoded: ", el
+    #Step 6: Receive Data 
+    for i in range(4):   
+	    fdata = chip['FIFO'].get_data()
+	    print fdata
+	    dout = chip.decode_fpga(fdata, True)
+	    print dout
+	    for i, d in enumerate(fdata):
+	        print i, hex(d), (d & 0x01000000) != 0, bin(d & 0xffffff), hex(d & 0xffffff)
+	        pretty_print(d)
+	    for el in dout:
+	        print "Decoded: ", el
+	    ddout=chip.decode(dout,1001)
+	    print ddout
+
 
