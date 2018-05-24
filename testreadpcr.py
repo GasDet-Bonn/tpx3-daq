b406f712851381c9dab0b7a1c6f6ada95fc5baa5
vedant8 <vedant8basu@gmail.com>
2018-05-24 13:34:55 +0200

    pllconfig test

--- testreadpcr.py
+++ testreadpcr.py
@@ -29,9 +29,7 @@ def main(args_dict):
     chip['CONTROL']['RESET'] = 0
     chip['CONTROL'].write()
 
-    # print 'RX ready:', chip['RX'].is_ready
-    # print 'get_decoder_error_counter', chip['RX'].get_decoder_error_counter()
-
+    
     data = chip.getGlobalSyncHeader() + [0x10] + [0b10101010, 0x01] + [0x00]
 
     chip.write(data)
@@ -45,84 +43,53 @@ def main(args_dict):
 
     while(not chip['RX'].is_ready):
         pass
-    data = chip.reset_sequential(write=False)
-    chip['FIFO'].reset()
-    time.sleep(0.01)
-    chip.write(data)
-    time.sleep(0.01)
-    print "reset sequential command sent" 
-    fdata = chip['FIFO'].get_data()
-    print fdata
-    dout = chip.decode_fpga(fdata, True)
-    print dout
-    ddout=chip.decode(dout[0],0x71)
-    print ddout    
-  
-       # Step 3: Set PCR
-    #Step 3a: Produce needed PCR
-    for x in range(256):
-        for y in range(256):
-            chip.set_pixel_pcr(x, y, 1, 7, 1)
-
-    # Step 3b: Write PCR to chip
-    for i in range(256):  
-      data = chip.write_pcr([i], write=False)
-      chip['FIFO'].reset()
-      time.sleep(0.01)
-      chip.write(data)
-      time.sleep(0.01)
-    print "pixel config sent"
-    fdata = chip['FIFO'].get_data()
-    print fdata
-    dout = chip.decode_fpga(fdata, True)
-    print dout
-    ddout=chip.decode(dout[0],0x71)
-    print ddout 
+        
     
-    data = chip.read_pixel_config_reg(0x02,write=False)
-    chip['FIFO'].reset()
-    time.sleep(0.01)
-    chip.write(data)
-    time.sleep(0.01)
-    print "read pixel config command sent"
-    fdata = chip['FIFO'].get_data()
-    print fdata
-    dout = chip.decode_fpga(fdata, True)
+    
+    # Step 4d: Reset and start Timer
+    print "ReSet Timer"
+    data = chip.resetTimer(write=False)
+    chip.write(data, True)
+    print "Start Timer"
+    data = chip.startTimer(write=False)
+    chip.write(data, True)
+    
+    # Step 5: Set general config
+    print "Set general config"
+    data = chip.write_general_config(write=False)
+    chip.write(data, True)
+    dout = chip.decode(chip.decode_fpga(chip['FIFO'].get_data(), True)[0], 0x71)
     print dout
-    ddout = chip.decode(dout[0], 0x71)
+
+
+    # Step 2a: reset sequential / resets pixels?!
+    data = chip.reset_sequential(False)
+    chip.write(data)
+    fdata = chip['FIFO'].get_data()
+    dout = chip.decode_fpga(fdata, True)
+    ddout = chip.decode(dout[0],0x71)
     print ddout
     
-    data = chip.read_pixel_matrix_sequential(0x02,False)
-    print "read matrix sequential command sent"
-    chip['FIFO'].reset()
-    time.sleep(0.01)
-    chip.write(data)
-    time.sleep(0.01)
-    print "waiting for packets received"
+    # Step 3b: Write PLL to chip
+    chip.write_pll_config(0,1,1,1,0,0,0,False)
+    print "pll config sent"
     fdata = chip['FIFO'].get_data()
-    print fdata
-<<<<<<< HEAD
-    print len(fdata)
     dout = chip.decode_fpga(fdata, True)
-   # print dout 
-    for i in range (3):
-      ddout = chip.decode(dout[i], 0x90)
-      print ddout
-    
-=======
+    print dout
+    sdata = chip.read_pll_config( write=False)
+    chip.write(data)
+    print "read pll config command sent"
+    fdata = chip['FIFO'].get_data()
     dout = chip.decode_fpga(fdata, True)
-    print dout 
-    #ddout = chip.decode(dout[0], 0x71)
-    #print ddout
-    #ddout = chip.decode(dout[1], 0x71)
-    #print ddout
->>>>>>> b8bc9bb19bc341a01eacc24288f9bd7907dbb573
-   
-     
+    print dout
+    ddout = chip.decode(dout[0], 0x21)
+    print ddout
+    ddout = chip.decode(dout[1], 0x71)
+    print ddout
+
     
 
-
-
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Timepix3 CTPR read/write checking script')
     args_dict = vars(parser.parse_args())
