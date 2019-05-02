22aeaec3de1a5d1ff449ecc86d3dbcb101e7c54b
Markus Gruber <gruber@physik.uni-bonn.de>
2019-05-02 19:18:10 +0200

    CI: add batch script to flash firmware with impact
    Currently a precompiled bit file is necessary. Its path must be "/home/tpc/Firmware/tpx3_fec6.bit".
    Because of this it is currently only working with the FEC6.

---
+++ firmware/flash_firmware.cmd
@@ -0,0 +1,11 @@
+setMode -bs
+setMode -bs
+setMode -bs
+setMode -bs
+setCable -port auto
+setCable -port auto
+Identify -inferir 
+identifyMPM 
+assignFile -p 1 -file "/home/tpc/Firmware/tpx3_fec6.bit"
+Program -p 1
+quit
