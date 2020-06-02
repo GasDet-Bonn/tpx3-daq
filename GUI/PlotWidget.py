28d4a8af08fefff3b8a15678524e1b55526a5a81
tschiffer <37073044+tschiffer@users.noreply.github.com>
2020-06-02 14:12:10 +0200

    GUI: Created PlotWidget.py

---
+++ GUI/PlotWidget.py
@@ -0,0 +1,135 @@
+
+#import gi
+
+#gi.require_version("Gtk", "3.0")
+#from gi.repository import Gtk
+#from gi.repository import GObject
+import numpy as np
+from matplotlib.figure import Figure
+from numpy import arange, pi, random, linspace
+import matplotlib.cm as cm
+from matplotlib.backends.backend_gtk3agg import (FigureCanvasGTK3Agg as FigureCanvas)
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+
+class plotwidget2(object):#For tests
+	def __init__(self):
+		self.active=True
+		self.fig = Figure(figsize=(5,5), dpi=100)
+		self.ax = self.fig.add_subplot(111, projection='polar')
+		
+		self.canvas = FigureCanvas(self.fig)
+		self.canvas.set_size_request(400,400)
+
+		N = 20
+		theta = linspace(0.0, 2 * pi, N, endpoint=False)
+		radii = 10 * random.rand(N)
+		width = pi / 4 * random.rand(N)
+
+		self.bars = self.ax.bar(theta, radii, width=width, bottom=0.0)
+		for r, bar in zip(radii, self.bars):
+			bar.set_facecolor(cm.jet(r / 10.))
+			bar.set_alpha(0.5)
+		self.ax.plot()
+	
+	def update_plot(self):
+		print("upsate")
+		self.ax.cla()
+		N = 20
+		theta = linspace(0.0, 2 * pi, N, endpoint=False)
+		radii = 10 * random.rand(N)
+		width = pi / 4 * random.rand(N)
+
+		self.bars = self.ax.bar(theta, radii, width=width, bottom=0.0)
+
+		for r, bar in zip(radii, self.bars):
+			bar.set_facecolor(cm.jet(r / 10.))
+			bar.set_alpha(0.5)
+		self.canvas.draw()
+		return True
+		
+class plotwidget(object):
+	def __init__(self):
+		self.fig = Figure(figsize=(5,5), dpi=100)
+		self.ax = self.fig.add_subplot()
+		self.ax.set_xlabel('X', size = 12)
+		self.ax.set_ylabel('Y', size = 12)
+		self.ax.axis([0,255,0,255])
+		self.x_vals = []
+		self.y_vals = []
+		self.t_vals = []
+		self.intensity = []
+		self.length = []
+		self.i=0
+		iterations = 100
+
+		t_vals = np.linspace(0,1, iterations)
+
+		#colors = [[0,0,1,0],[0,0,1,0.5],[0,0.2,0.4,1]]
+		#cmap = LinearSegmentedColormap.from_list("", colors)
+		cmap =self.fading_colormap(50)
+		self.scatter = self.ax.scatter(self.x_vals,self.y_vals, c=[], cmap=cmap, vmin=0,vmax=1)
+
+		self.canvas = FigureCanvas(self.fig)
+		self.canvas.set_size_request(400,400)
+		
+		self.ax.plot()
+	
+	def fading_colormap(self,steps=50):
+		self.colorsteps = steps
+		i=1
+		viridis = cm.get_cmap("viridis", 256)
+		newcmap = viridis(np.linspace(0, 1, 256))
+		newmap1 = np.tile(newcmap, (self.colorsteps,1))
+		while(i<self.colorsteps):
+			newmap1[(i-1)*256:(i*256),-1] = np.linspace(i*1/self.colorsteps, i*1/self.colorsteps, 256)
+			i= i+1
+		cmap =ListedColormap(newmap1)
+		return cmap
+		
+	def get_new_vals(self):
+		#Get values from Chip
+		#
+		#t need to between 0 and 1 then the calculation 1-(t/self.colorsteps) needs 
+		#to be done in order to distribute is correctly over the colormap
+		
+		n = np.random.randint(1,5)
+		x = (np.random.rand(n)*255)
+		y = (np.random.rand(n)*255)
+		t = (1-(np.random.rand(n)/self.colorsteps))
+
+		return list(x), list(y),list(t)
+
+	def update_plot(self):
+		new_xvals, new_yvals, new_tvals = self.get_new_vals()
+		self.x_vals.extend(new_xvals)
+		self.y_vals.extend(new_yvals)
+		self.t_vals.extend(new_tvals)
+		self.length.append(len(new_xvals))
+		
+		#Cut plotting arrays to 50 Timeblocks
+		if self.i<=(self.colorsteps-1):
+			self.i =self.i+1
+		elif self.i>(self.colorsteps-1):
+			n=0
+			number = self.length[0]
+			self.length.pop(0)
+			while n<number:
+				n=n+1
+				self.x_vals.pop(0)
+				self.y_vals.pop(0)
+				self.t_vals.pop(0)
+				self.intensity = np.delete(self.intensity,0)
+		
+		#Add to plot and change intensity
+		self.scatter.set_offsets(np.c_[self.x_vals,self.y_vals])
+
+		self.intensity = np.concatenate((np.array(self.intensity)-(1/self.colorsteps), new_tvals))
+
+		print (self.intensity)
+		self.scatter.set_array(self.intensity)
+		
+		self.canvas.draw()
+		
+		#print("update")
+		return True
+				
