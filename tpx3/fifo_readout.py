5079b99acb8b1a5da12b3fb7f6ce6505f3464da0
Markus Gruber <gruber@physik.uni-bonn.de>
2021-04-06 10:35:09 +0200

    FIX: fix error reset during readout
    If a discard or decode error occurs during radout the error counter should be
    reset. Before this commit the receiver was reset instead which caused a stop
    of the readout.

--- tpx3/fifo_readout.py
+++ tpx3/fifo_readout.py
@@ -226,7 +226,8 @@ class FifoReadout(object):
                 if n_words == 0 and self.stop_readout.is_set():
                     break
                 if discard_error > 0 or decode_error > 0:
-                    self.reset_rx()
+                    self.logger.warning('There were {} discard errors and {} decode errors - Resetting error counters'.format(discard_error + decode_error))
+                    self.rx_error_reset()
             finally:
                 time_wait = self.readout_interval - (time() - time_read)
             if self._calculate.is_set():
