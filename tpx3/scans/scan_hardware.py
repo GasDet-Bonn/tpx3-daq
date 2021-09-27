702d9f4f13bd73dca0f2c003d0e90c33cbebeef0
Markus Gruber <gruber@physik.uni-bonn.de>
2021-09-27 16:20:21 +0200

    ENH: add link status to scan_hardware

--- tpx3/scans/scan_hardware.py
+++ tpx3/scans/scan_hardware.py
@@ -72,23 +72,42 @@ class ScanHardware(object):
                 rx_map[chip_link][fpga_link_number] = self.chip[fpga_link].is_ready
 
         status_map = np.sum(rx_map, axis = 0)
+        status_map = np.clip(status_map, 0, 1)
+
+        for fpga_link_number, fpga_link in enumerate(rx_list_names):
+            self.chip._outputBlocks["chan_mask"] = 0b00000000
+            data = self.chip.write_outputBlock_config()
+            self.chip[fpga_link].ENABLE = 0
+
+        # Test if links see data when everything is switched off
+        noisy_map = np.zeros(8, np.int8)
+        for i in range(500):
+            for fpga_link_number, fpga_link in enumerate(rx_list_names):
+                noisy_map[fpga_link_number] += self.chip[fpga_link].is_ready
+
+        # Mark links that show data when everything is off as borken zero patter (status 6)
+        for fpga_link_number, fpga_link in enumerate(rx_list_names):
+            if noisy_map[fpga_link_number] > 0:
+                status_map[fpga_link_number] = 6
 
         # Check for each link individually for which delay settings there are no errors
         for fpga_link_number, fpga_link in enumerate(rx_list_names):
-            self.chip._outputBlocks["chan_mask"] = 0b1 << int(np.where(rx_map == 1)[0][fpga_link_number])
-            data = self.chip.write_outputBlock_config()
+            # Do this check only on links which are connected and show no errors sofar
+            if status_map[fpga_link_number] == 1:
+                self.chip._outputBlocks["chan_mask"] = 0b1 << int(np.where(rx_map == 1)[0][fpga_link_number])
+                data = self.chip.write_outputBlock_config()
 
-            for delay in range(32):
-                self.chip[fpga_link].ENABLE = 0
-                self.chip[fpga_link].reset()
-                self.chip[fpga_link].ENABLE = 1
-                self.chip[fpga_link].DATA_DELAY = delay
-                self.chip[fpga_link].INVERT = 0
-                self.chip[fpga_link].SAMPLING_EDGE = 0
+                for delay in range(32):
+                    self.chip[fpga_link].ENABLE = 0
+                    self.chip[fpga_link].reset()
+                    self.chip[fpga_link].ENABLE = 1
+                    self.chip[fpga_link].DATA_DELAY = delay
+                    self.chip[fpga_link].INVERT = 0
+                    self.chip[fpga_link].SAMPLING_EDGE = 0
 
-                # Check the number of errors for the current setting
-                error_map[fpga_link_number][delay] = self.chip[fpga_link].get_decoder_error_counter()
-                self.chip[fpga_link].ENABLE = 0
+                    # Check the number of errors for the current setting
+                    error_map[fpga_link_number][delay] = self.chip[fpga_link].get_decoder_error_counter()
+                    self.chip[fpga_link].ENABLE = 0
 
             if progress == None:
                 # Update the progress bar
@@ -102,15 +121,20 @@ class ScanHardware(object):
         # Find for each receiver the delay with the most distance to a delay with errors
         delays = np.zeros(8, dtype=np.int8)
         for receiver in range(8):
-            zero_error_map = np.where(error_map[receiver] == 0)[0]
-            zero_delays = np.split(zero_error_map, np.where(zero_error_map[:-1] != zero_error_map[1:] - 1)[0])
-            list_index = np.argmax(np.array([zero_delays[i].size for i in range(len(zero_delays))]))
-            delays[receiver] = zero_delays[list_index][int(np.median(zero_delays[list_index]))]
+            if status_map[fpga_link_number] == 1:
+                zero_error_map = np.where(error_map[receiver] == 0)[0]
+                # If there is no delay without errors set the link status to 4 (Connected, No delay without errors, Off)
+                if len(zero_error_map) == 0:
+                    status_map[receiver] = 4
+                    continue
+                zero_delays = np.split(zero_error_map, np.where(zero_error_map[:-1] != zero_error_map[1:] - 1)[0])
+                list_index = np.argmax(np.array([zero_delays[i].size for i in range(len(zero_delays))]))
+                delays[receiver] = zero_delays[list_index][int(np.median(zero_delays[list_index]))]
 
         # Check for each receiver the ChipID of the connected chip
         Chip_IDs = np.zeros(8, dtype=np.int32)
         for fpga_link_number, fpga_link in enumerate(rx_list_names):
-            if status_map[fpga_link_number] != 1:
+            if status_map[fpga_link_number] == 0:
                 Chip_IDs[fpga_link_number] = 0
                 continue
 
@@ -160,6 +184,7 @@ class ScanHardware(object):
             register['data-delay'] = int(delays[i])
             register['data-invert'] = 0
             register['data-edge'] = 0
+            register['link-status'] = int(status_map[i])
 
         # Write the ideal settings to the yaml file
         with open(yaml_file, 'w') as file:
@@ -177,7 +202,7 @@ class ScanHardware(object):
             ID = 'W' + str(wafer_number) + '-' + x_position + str(y_position)
 
             # Write new Chip-ID to the list
-            if ID not in ID_List and register['chip-id'] != 0:
+            if ID not in ID_List:
                 ID_List.append([register['chip-id'], ID])
 
         # Create a list of Chips with all link settings for the specific chip
@@ -188,13 +213,13 @@ class ScanHardware(object):
                 if ID[0] == register['chip-id']:
                     # If the list is empty or the current chip is not in the list add it with its settings
                     if len(Chip_List) == 0 or ID[1] not in Chip_List[:][0]:
-                        Chip_List.append([ID[1], [register['fpga-link'], register['chip-link'], register['data-delay'], register['data-invert'], register['data-edge']]])
+                        Chip_List.append([ID[1], [register['fpga-link'], register['chip-link'], register['data-delay'], register['data-invert'], register['data-edge'], register['link-status']]])
 
                     # If the Chip is already in the list just add the link settings to it
                     else:
                         for chip in Chip_List:
                             if ID[1] == chip[0]:
-                                chip.append([register['fpga-link'], register['chip-link'], register['data-delay'], register['data-invert'], register['data-edge']])
+                                chip.append([register['fpga-link'], register['chip-link'], register['data-delay'], register['data-invert'], register['data-edge'], register['link-status']])
                     break
 
         if results == None:
